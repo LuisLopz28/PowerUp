155a202ae7f442440435d0e35bb35b8264ee3780
LuisLopz28 <luislopez.idetp@gmail.com>
2025-06-13 10:18:44 -0500

    actualizacion1

--- auth/login.py
+++ auth/login.py
@@ -1,23 +1,21 @@
 # auth/login.py
 import streamlit as st
-
-USERS = {
-    "admin": "admin123",
-    "luis": "powerup2025"
-}
+from database.db import verificar_usuario
 
 def login():
     if "logged_in" not in st.session_state:
         st.session_state.logged_in = False
 
     if not st.session_state.logged_in:
-        st.title("🔒 Iniciar sesión")
-        user = st.text_input("Usuario")
+        st.title("🔐 Iniciar sesión")
+        username = st.text_input("Usuario")
         password = st.text_input("Contraseña", type="password")
+
         if st.button("Ingresar"):
-            if USERS.get(user) == password:
+            if verificar_usuario(username, password):
                 st.session_state.logged_in = True
+                st.success("¡Bienvenido!")
                 st.experimental_rerun()
             else:
-                st.error("Credenciales inválidas")
+                st.error("Usuario o contraseña inválidos")
         st.stop()
