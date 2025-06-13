d33ac2443940b5d359d06a6fb8dde3a082b9f00c
LuisLopz28 <luislopez.idetp@gmail.com>
2025-06-13 10:54:54 -0500

    actualizacion2

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from routes import home, powerbi, analisis, simulador, estado, auth
+from flask import redirect, url_for, session, request
 
+@app.before_request
+def require_login():
+    rutas_libres = ['auth.login', 'auth.register', 'static']  # si tienes un registro
+    if 'user' not in session and request.endpoint not in rutas_libres:
+        return redirect(url_for('auth.login'))
 app = Flask(__name__)
 app.secret_key = 'tu_clave_secreta'
 
