bc97ef63eaacbce2bed24a78e70dc9f9080a9854
LuisLopz28 <luislopez.idetp@gmail.com>
2025-06-13 10:26:07 -0500

    act2

--- components/navbar.py
+++
@@ -1,9 +0,0 @@
-# components/navbar.py
-import streamlit as st
-
-def menu():
-    return st.sidebar.radio("📂 Navegación", [
-        "Inicio",
-        "Simulador",
-        "Estado de Resultado"
-    ])
