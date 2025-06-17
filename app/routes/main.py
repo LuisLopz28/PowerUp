a4d79826871768639cfec49cbf05fc5149e23dab
LuisLopz28 <luislopez.idetp@gmail.com>
2025-06-17 00:02:53 -0500

    aa

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,16 +1,11 @@
-# app/routes/auth.py
+# app/routes/main.py
 
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required
+from flask import Blueprint, render_template
+from flask_login import login_required
 
-auth_bp = Blueprint('auth', __name__)
+main_bp = Blueprint('main', __name__)
 
-@auth_bp.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('auth/login.html')
-
-@auth_bp.route('/logout')
+@main_bp.route('/')
 @login_required
-def logout():
-    logout_user()
-    return redirect(url_for('main.index'))
+def index():
+    return render_template('main/index.html')
