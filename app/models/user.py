987d1ceebf3a922ad728c4b9d26f8fcb62f0758b
LuisLopz28 <luislopez.idetp@gmail.com>
2025-06-16 23:27:25 -0500

    act2

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from flask_login import UserMixin
 
 # Simulación de base de datos en memoria
 users = {
-    "admin": {"password": "scrypt:32768:8:1$KKIIPaJigA0uv3Jf$e2cdd8511a657cb8cbf8d288dc58474e8e926cfe2c52ce8fdc2603676395bdedfd7a9c60d931fb14a6f5611b5c120d9d7e8e137d345970feb7571d1405c2f9a2", "id": 1}  # Cambiar por hash real
+    "admin": {"password": "pbkdf2:sha256:1000000$RIx2qllYCbrg21aM$a37273354dcc85f9270bb1dbd89abdd8c17a6d39ba05c340cd8b8bc67a73c51d", "id": 1}  # Cambiar por hash real
 }
 
 class User(UserMixin):
