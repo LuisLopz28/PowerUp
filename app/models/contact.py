46a3d221e64013189b2d4e92b334b54e8157b38c
LuisLopz28 <luislopez.idetp@gmail.com>
2025-06-17 00:33:37 -0500

    delete

--- app/models/contact.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from run import db
-
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    message = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = 'tusecretokey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'powerup.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
